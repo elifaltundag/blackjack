e7f6776b13109b5746bb9352d8945cba9db9e51a
elifaltundag <elif.altundag.x@gmail.com>
2022-04-25 17:15:02 +0300

    Get number of players, their names and deal them each 2 cards

--- blackjack.py
+++ blackjack.py
@@ -2,32 +2,74 @@
 Basic terminal program of the game Blackjack
 """
 
-# PLAYER CLASS
-class Player:
-    def __init__(self, name = "PlayerX", sum_cards = 0):
-        self.name = name
-        self.sum_cards = sum_cards
+import random
 
-    def __repr__(self):
-        return self.name + " has a total of " + str(self.sum_cards)
+# CARDS 
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+values = []
+""" for card in cards:
+    if isinstance(card, int):
+        values.append(card)
+    elif isinstance(card, str):
+        values.append(11) """
+
+
 
 # FUNCTIONS
-def get_players():
+def get_players_names():
     print("Welcome to Blackjack Casino by Elif!")
     num_players = int(input("Enter number of players: "))
     
     player_names = []
     for i in range(num_players):
         player_names.append(input(f"Player{i+1}, enter your nickname: "))
-    print(player_names)
     return player_names
 
 
-        
+def deal_first_cards():
+    num_first_cards = 2
+    first_cards = []
+    for i in range(num_first_cards):   
+        first_cards.append(random.choice(cards))
+    return first_cards
+
+
+def calculate_sum(cards):
+    sum_cards = 0
+    for card in cards:
+        if card != "A":
+            if isinstance(card, int):
+                sum_cards += card
+            elif isinstance(card, str):
+                sum_cards += 10
+        elif card == "A":
+            if sum_cards + 11 > 21:
+                sum_cards += 1
+            elif sum_cards + 11 <= 21:
+                sum_cards += 11
+    return sum_cards
+
+
+# PLAYER CLASS
+class Player:
+    def __init__(self, name = "PlayerX"):
+        self.name = name
+        self.cards = deal_first_cards()
+        self.sum_cards = calculate_sum(self.cards)
+
+    def __repr__(self):
+        return f"{self.name}'s cards: {self.cards} => total: {self.sum_cards}"  
 
 
 
 # CONTROL FLOW
-get_players()
-elifing = Player()
-print(elifing)
+starting = get_players_names()
+
+players = []
+for player_name in starting:
+    player_name = Player(player_name)
+    players.append(player_name)
+
+for player in players:
+    print(player)
+
